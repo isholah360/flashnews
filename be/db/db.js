a769ca4b612a50c2ee0a671841afbedeaa19787a
ishola360 <isholah360.com>
2024-08-03 17:39:21 +0100

    mongoChange

--- be/db/db.js
+++ be/db/db.js
@@ -6,8 +6,8 @@ const blogDb = async () => {
         // const onlineDbUrl = "mongodb+srv://babalolah360:BABAlola360@dates.vkqmldt.mongodb.net/dating?retryWrites=true&w=majority&appName=dates";
         // await mongoose.connect(onlineDbUrl);
         // console.log(`dating app Server is connecetd to database`) 
-        // const konect = await mongoose.connect(process.env.MONGO_URI)
-        const konect = await mongoose.connect("mongodb://127.0.0.1:27017/blog")
+        const konect = await mongoose.connect(process.env.MONGO_URI)
+        // const konect = await mongoose.connect("mongodb://127.0.0.1:27017/blog")
         console.log(`blog app Server is connecetd to ${konect.connection.host}`)
         
     } catch (error) {
