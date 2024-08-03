0d35ce901a6a50d42e79c539732e7f93f1d44940
ishola360 <isholah360.com>
2024-08-03 21:57:51 +0100

    newtarget

--- feb/vite.config.js
+++ feb/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target: "http://localhost:5000",
+        target: "https://flashnews-y0dp.onrender.com",
         changeOrigin: true
       }
     }
