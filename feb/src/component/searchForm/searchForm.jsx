b2b70f278d0ddb167965b874b99d9cd68af2dd64
ishola360 <isholah360.com>
2024-08-03 19:40:39 +0100

    axiorm

--- feb/src/component/searchForm/searchForm.jsx
+++ feb/src/component/searchForm/searchForm.jsx
@@ -1,25 +1,45 @@
 import React, { useState } from "react";
-import axios from "axios";
 import { useNavigate  } from "react-router-dom";
 
 function SearchForm() {
     const [query, setQuery] = useState('');
     const navigate = useNavigate();
   
+    // const handleSearch = async (e) => {
+    //   e.preventDefault();
+    //   try {
+    //     const response = await axios.get(`http://localhost:5000/api/post/all?search=${query}`);
+    //     if (response.data) {
+    //       navigate('/results', { state: { searchResults: response.data, query } });
+    //       console.log(response.data)
+    //     } else {
+    //       console.warn('No results found');
+    //     }
+    //   } catch (error) {
+    //     console.error('Error fetching search results:', error);
+    //   }
+    // };
     const handleSearch = async (e) => {
       e.preventDefault();
       try {
-        const response = await axios.get(`http://localhost:5000/api/post/all?search=${query}`);
-        if (response.data) {
-          navigate('/results', { state: { searchResults: response.data, query } });
-          console.log(response.data)
+        const response = await fetch(`http://localhost:5000/api/post/all?search=${query}`);
+        
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        
+        const data = await response.json();
+        
+        if (data) {
+          navigate('/results', { state: { searchResults: data, query } });
+          console.log(data);
         } else {
           console.warn('No results found');
         }
       } catch (error) {
         console.error('Error fetching search results:', error);
       }
-    };
+    }
   return (
     <div>
       <div className="search-form">
