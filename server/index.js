63b5838e7805d8f1c6687ef1d6350f8bcae484e5
ishola360 <isholah360.com>
2024-08-03 21:31:00 +0100

    slashPath

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ app.use('/api/cat', router)
 
 
 
-app.use(express.static(path.join(__dirname, '/feb/dist')))
+app.use(express.static(path.join(__dirname, '/../feb/dist')))
 
 app.get('*', (req, res)=>{
   res.sendFile(path.join(__dirname, '..', 'feb','dist','index.html'))
